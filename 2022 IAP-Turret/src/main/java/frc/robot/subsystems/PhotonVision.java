b0b27b10e0fe714b7781cd35b83196757f99d041
RossBrown2718 <115900480+NathanS54@users.noreply.github.com>
2022-11-10 17:10:15 -0800

    Commands
    We finished the commands for the spintoturret and i finished the rest of the subsystems and finished some of driverain

--- 2022 IAP-Turret/src/main/java/frc/robot/subsystems/PhotonVision.java
+++ 2022 IAP-Turret/src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -4,14 +4,74 @@
 
 package frc.robot.subsystems;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PhotonVision extends SubsystemBase {
   /** Creates a new PhotonVision. */
-  public PhotonVision() {}
+  
+    
+  private PhotonCamera camera;
+  private PhotonTrackedTarget target;
+
+  private boolean hasTarget;
+  private double yaw;
+  private double pitch;
+  private double area;
+  private double skew;
+  
+  public PhotonVision() {
+    PhotonCamera camera = new PhotonCamera("photovision");
+    camera.setDriverMode(true);
+    camera.setPipelineIndex(2);
+  }
+
+   public boolean targetExists(){
+     return hasTarget;
+   }
+    
+   public double getYaw(){
+     return yaw;
+   }
+
+   public double getPitch(){
+      return pitch;
+    
+   }
+    
+   public double getArea(){
+     return area;
+   }
+    
+   public double getSkew(){
+     return skew;
+   }
+
+    
+    
+    
+
+
+
+  
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    var result = camera.getLatestResult();
+     hasTarget = result.hasTargets();
+    if (hasTarget){
+      target = result.getBestTarget();
+      yaw = target.getYaw();
+      pitch = target.getPitch();
+      area = target.getArea();
+      skew = target.getSkew();
+    }
+     SmartDashboard.putNumber("Yaw,", yaw);
+     SmartDashboard.putNumber("Pitch",pitch);
+     SmartDashboard.putNumber("Area",area);
+     SmartDashboard.putNumber("Skew", skew);
   }
 }
